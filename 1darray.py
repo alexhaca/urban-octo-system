0482321c2ee480d6496234451b9e0314118ca589
37081 <me@me.com>
2017-11-07 14:18:49 +0000

    fghfghgf

--- 1darray.py
+++ 1darray.py
@@ -1,6 +1,12 @@
 # DECLARE ages : ARRAY[0...9] OF INTEGER
-ages = [12,21,35,65,42,1,5,24,14,12]
-
+# ages = [12,21,35,65,42,1,5,24,14,12]
+ages = []
+for index in range(0,10):
+temp = int(input("enter age between 0-99"))
+while temp < 0 or temp > 99:
+    print("idiot!, you didnt enter the number correctly.")
+    temp = int(input("enter age corrctly between 0-99"))
+    ages.append(temp)
 for index in range(0,10):
     print (ages [index])
 
